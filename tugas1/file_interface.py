069d2b7d3a1cba3494a34b1ab68ce561a49eda72
rizqitsani <rizqitsani@gmail.com>
2022-06-08 20:19:12 +0700

    feat: add upload and delete protocol

--- tugas1/file_interface.py
+++ tugas1/file_interface.py
@@ -6,28 +6,67 @@ from glob import glob
 
 class FileInterface:
     def __init__(self):
-        os.chdir('files/')
+        os.chdir("files/")
 
-    def list(self):
+    def list(self, params=[]):
         try:
-            filelist = glob('*.*')
-            return dict(status='OK',data=filelist)
+            filelist = glob("*.*")
+            return dict(status="OK", data=filelist)
         except Exception as e:
-            return dict(status='ERROR',data=str(e))
+            return dict(status="ERROR", data=str(e))
 
-    def get(self,filename=''):
-        if(filename==''):
-            return None
+    def get(self, params=[]):
         try:
-            fp = open(f"{filename}",'rb')
+            filename = params[0]
+            if filename == "":
+                return None
+            fp = open(f"{filename}", "rb")
             isifile = base64.b64encode(fp.read()).decode()
-            return dict(status='OK',data_namafile=filename,data_file=isifile)
+            return dict(status="OK", data_namafile=filename, data_file=isifile)
         except Exception as e:
-            return dict(status='ERROR',data=str(e))
+            return dict(status="ERROR", data=str(e))
+
+    def post(self, params=[]):
+        try:
+            filename = params[0]
+            isifile = params[1]
+            if filename == "":
+                return None
+
+            if os.path.exists(f"{filename}"):
+                return dict(
+                    status="ERROR", data=f"File {filename} sudah ada pada server"
+                )
+
+            file = base64.b64decode(isifile)
+
+            fp = open(filename, "wb+")
+            fp.write(file)
+            fp.close()
+
+            return dict(status="OK", data="File berhasil ditambahkan")
+
+        except Exception as e:
+            return dict(status="ERROR", data=str(e))
+
+    def delete(self, params=[]):
+        try:
+            filename = params[0]
+
+            if not os.path.exists(filename):
+                return dict(
+                    status="ERROR", data=f"File {filename} tidak ada pada server"
+                )
+
+            os.remove(filename)
+
+            return dict(status="OK", data=f"File {filename} berhasil dihapus")
+
+        except Exception as e:
+            return dict(status="ERROR", data=str(e))
 
 
-
-if __name__=='__main__':
+if __name__ == "__main__":
     f = FileInterface()
     print(f.list())
-    print(f.get('pokijan.jpg'))
+    print(f.get(["pokijan.jpg"]))
--- tugas1/file_protocol.py
+++ tugas1/file_protocol.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import shlex
 
 from file_interface import FileInterface
 
@@ -16,29 +17,25 @@ string
 """
 
 
-
 class FileProtocol:
     def __init__(self):
         self.file = FileInterface()
-    def proses_string(self,string_datamasuk=''):
+
+    def proses_string(self, string_datamasuk=""):
         logging.warning(f"string diproses: {string_datamasuk}")
-        c = string_datamasuk.split(" ")
+        c = shlex.split(string_datamasuk.lower())
         try:
             c_request = c[0].strip()
             logging.warning(f"memproses request: {c_request}")
-            if (c_request=='LIST'):
-                return json.dumps(self.file.list())
-            elif (c_request=='GET'):
-                param1 = c[1].strip()
-                return json.dumps(self.file.get(param1))
-            else:
-                return json.dumps(dict(status='ERROR', data='request tidak dikenali'))
+            params = [x for x in c[1:]]
+            cl = getattr(self.file, c_request)(params)
+            return json.dumps(cl)
         except Exception:
-            return json.dumps(dict(status='ERROR',data='request tidak dikenali'))
+            return json.dumps(dict(status="ERROR", data="request tidak dikenali"))
 
 
-if __name__=='__main__':
-    #contoh pemakaian
+if __name__ == "__main__":
+    # contoh pemakaian
     fp = FileProtocol()
     print(fp.proses_string("LIST"))
     print(fp.proses_string("GET pokijan.jpg"))
